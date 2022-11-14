28ced521c519fb0f283b92101528e7dee8bf16de
vikranthsingh <vikranth.civilenginer@gmail.com>
2022-11-14 17:40:42 +0530

    Again 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
+class Test {
+    void m1(int i) {
+        System.out.println("int arg method");
+    }
 
+    void m1(float f) {
+        System.out.println("float arg method");
+    }
+
+}
 
 public class Main {
     public static void main(String[] args) {
-
+        Test t = new Test();
+        t.m1('C');
+        t.m1(10l);
     }
 }
